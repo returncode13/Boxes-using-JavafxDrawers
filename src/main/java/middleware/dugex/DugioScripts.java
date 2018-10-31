dc9c8039b7a34d7f66f7aac383c32a37e05b81a9
sharath <returncode13>
2018-11-01 01:23:54 +0530

    accounting for dugio2

--- src/main/java/middleware/dugex/DugioScripts.java
+++ src/main/java/middleware/dugex/DugioScripts.java
@@ -23,6 +23,7 @@ public class DugioScripts implements Serializable{
     private File dugioHeaderValues;
     private File getTimeSubsurfaces;
     private File dugioGetTraces;
+    private File dugio2GetTraces;
     private File subsurfaceLog;
     private File logStatusCompletedSuccessfully;
     private File logStatusErrored;
@@ -50,8 +51,18 @@ public class DugioScripts implements Serializable{
     private String dugioHeaderValuesContent="#!/bin/bash\n"
             + "module add prod\n"
             + "dugio md_get file=$1 line=$2 key=$3";
-    private String dugioTracesContent = "#!/bin/bash\n"
+    private String dugioTracesContent="#!/bin/bash\n" +
+                                        "count = `ls -1 $1/*.idb 2>/dev/null | wc -l`\n" +
+                                        "if [ count!=0 ]\n" +
+                                        "then\n" +
+                                        "dugio2 summary file=$1 line=$2 | grep  Traces |grep -oP [[:digit:],]+|head -1\n" +
+                                        "else\n" +
+                                        "dugio summary file=$1 line=$2 | grep  Traces |grep -oP [[:digit:]]+|head -1\n" +
+                                        "fi";
+            /* private String dugioTracesContent = "#!/bin/bash\n"
             + "dugio summary file=$1 line=$2 | grep  Traces |grep -oP [[:digit:]]+|head -1";
+            private String dugio2TracesContent="#!/bin/bash\n"
+            + "dugio2 summary file=$1 line=$2 | grep  Traces |grep -oP [[:digit:],]+|head -1";*/
             
            // + "dugio summary file=$1 line=$2 | grep  Traces |grep -oP '\\d+\\s+'\n";
     
@@ -234,6 +245,19 @@ public class DugioScripts implements Serializable{
             
             
             dugioGetTraces.deleteOnExit();
+        } catch (IOException ex) {
+            Logger.getLogger(DugioScripts.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+         try {
+            dugio2GetTraces=File.createTempFile("dugio2TracesContent", ".sh");
+            BufferedWriter bw = new BufferedWriter(new FileWriter(dugio2GetTraces));
+            bw.write(dugio2TracesContent);
+            bw.close();
+            dugio2GetTraces.setExecutable(true,false);
+            
+            
+            dugio2GetTraces.deleteOnExit();
         } catch (IOException ex) {
             Logger.getLogger(DugioScripts.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -459,6 +483,11 @@ public class DugioScripts implements Serializable{
         return dugioGetTraces;
     }
 
+    public String getDugio2TracesContent() {
+        return dugio2TracesContent;
+    }
+    
+    
     public File getSubsurfaceLog() {
         return subsurfaceLog;
     }
